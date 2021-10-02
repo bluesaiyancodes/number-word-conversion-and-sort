4f142bde8ad38ae4eea0ca496c17c1ae7ae68fe6
JJ0023 <jeevanjyotisahoo1@gmail.com>
2021-10-02 17:00:52 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -22,6 +22,7 @@ def merge(a, b):
 
 def mergesort(x):
     """ Function to sort an array using merge sort algorithm """
+    print("program started")
     if len(x) == 0 or len(x) == 1:
         return x
     else:
